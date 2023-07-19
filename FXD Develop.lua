60e69eec549252f30399767c886c0c930c910d08
BryanChi <93679021+BryanChi@users.noreply.github.com>
2023-07-19 16:09:50 +0900

    fix

--- FXD Develop.lua
+++ FXD Develop.lua
@@ -104,7 +104,9 @@ Df={ V_Sldr_W = 15;  KnobRadius=18; KnobSize= 15*3; Sldr_W=160 ; Dvdr_Width = 15
 AllAvailableKeys={['0']= r.ImGui_Key_0();['1']=r.ImGui_Key_1();['2']=r.ImGui_Key_2();['3']=r.ImGui_Key_3();['4']=r.ImGui_Key_4();['5']=r.ImGui_Key_5();['6']=r.ImGui_Key_6();['7']=r.ImGui_Key_7();['8']=r.ImGui_Key_8();['9']=r.ImGui_Key_9();
 A=r.ImGui_Key_A();B=r.ImGui_Key_B();C=r.ImGui_Key_C();D=r.ImGui_Key_D();E=r.ImGui_Key_E();F=r.ImGui_Key_F();G=r.ImGui_Key_G();H=r.ImGui_Key_H();I=r.ImGui_Key_I();J=r.ImGui_Key_J();K=r.ImGui_Key_K();L=r.ImGui_Key_L();M=r.ImGui_Key_M();N=r.ImGui_Key_N();O=r.ImGui_Key_O();P=r.ImGui_Key_P();Q=r.ImGui_Key_Q();R=r.ImGui_Key_R();S=r.ImGui_Key_S();T=r.ImGui_Key_T();U=r.ImGui_Key_U();V=r.ImGui_Key_V();W=r.ImGui_Key_W();X=r.ImGui_Key_X();Y=r.ImGui_Key_Y();Z=r.ImGui_Key_Z();
 Esc= r.ImGui_Key_Escape();F1=r.ImGui_Key_F1();F2=r.ImGui_Key_F2();F3=r.ImGui_Key_F3();F4=r.ImGui_Key_F4();F5=r.ImGui_Key_F5();F6=r.ImGui_Key_F6();F7=r.ImGui_Key_F7();F8=r.ImGui_Key_F8();F9=r.ImGui_Key_F9();F10= r.ImGui_Key_F10();F11=r.ImGui_Key_F11();F12=r.ImGui_Key_F12();
-Apostrophe = r.ImGui_Key_Apostrophe();Backslash = r.ImGui_Key_Backslash(); Backspace = r.ImGui_Key_Backspace(); Comma = r.ImGui_Key_Comma(); Delete = r.ImGui_Key_Delete() ; DownArrow = r.ImGui_Key_DownArrow(); Equal = r.ImGui_Key_Equal();Minus = r.ImGui_Key_Minus(); LeftArrow = r.ImGui_Key_LeftArrow();Period = r.ImGui_Key_Period();RightArrow = r.ImGui_Key_RightArrow(); SemiColon= r.ImGui_Key_Semicolon();Slash = r.ImGui_Key_Slash();Space=r.ImGui_Key_Space();UpArrow = r.ImGui_Key_UpArrow();
+Apostrophe = r.ImGui_Key_Apostrophe();Backslash = r.ImGui_Key_Backslash(); Backspace = r.ImGui_Key_Backspace(); Comma = r.ImGui_Key_Comma(); Delete = r.ImGui_Key_Delete() ; DownArrow = r.ImGui_Key_DownArrow(); Enter = r.ImGui_Key_Enter();End = r.ImGui_Key_End(); Equal = r.ImGui_Key_Equal(); GraveAccent = r.ImGui_Key_GraveAccent() ;Home = r.ImGui_Key_Home(); ScrollLock = r.ImGui_Key_ScrollLock();Insert = r.ImGui_Key_Insert();Minus = r.ImGui_Key_Minus(); LeftArrow = r.ImGui_Key_LeftArrow();LeftBracket = r.ImGui_Key_LeftBracket();Period = r.ImGui_Key_Period(); PageDown = r.ImGui_Key_PageDown();PageUp = r.ImGui_Key_PageUp();Pause = r.ImGui_Key_Pause();RightBracket = r.ImGui_Key_RightBracket() ;RightArrow = r.ImGui_Key_RightArrow(); SemiColon= r.ImGui_Key_Semicolon();Slash = r.ImGui_Key_Slash();Space=r.ImGui_Key_Space();Tab = r.ImGui_Key_Tab(); UpArrow = r.ImGui_Key_UpArrow();
+Pad0=r.ImGui_Key_Keypad0(); Pad1=r.ImGui_Key_Keypad1();Pad2=r.ImGui_Key_Keypad2();Pad3=r.ImGui_Key_Keypad3();Pad4=r.ImGui_Key_Keypad4();Pad5 = r.ImGui_Key_Keypad5(); Pad6=r.ImGui_Key_Keypad6();Pad7 = r.ImGui_Key_Keypad7(); Pad8=r.ImGui_Key_Keypad8();Pad9=r.ImGui_Key_Keypad9();
+PadAdd= r.ImGui_Key_KeypadAdd(); PadDecimal =r.ImGui_Key_KeypadDecimal();PadDivide=r.ImGui_Key_KeypadDivide();PadEnter = r.ImGui_Key_KeypadEnter();PadEqual=r.ImGui_Key_KeypadEqual();PadMultiply=r.ImGui_Key_KeypadMultiply();PadSubtract = r.ImGui_Key_KeypadSubtract();
 }
 KB_Shortcut = {}    
 Command_ID = {}
@@ -731,16 +733,18 @@ if visible  then
                 
 
                  
-                if r.ImGui_IsKeyPressed(ctx,  AllAvailableKeys[v] )  then 
+                if r.ImGui_IsKeyPressed(ctx,  AllAvailableKeys[v] ) and Mods == 0  then 
                     --[[ local commandID = r.NamedCommandLookup('_BR_FOCUS_ARRANGE_WND')
                     local CommandTxt =  r.CF_GetCommandText(0, commandID) -- 0 prob means arrange window, it's the section drop down from action window's top right corner
                     r.Main_OnCommand(commandID, 0) ]]
+                    msg('A '..v.. '\n')
                     if Command_ID[i] then 
                         local Cmd_Num = r.NamedCommandLookup(Command_ID[i])
                         r.Main_OnCommand(Cmd_Num, 0)
-                    end
+                    end 
 
                 end
+                
             else
 
 
@@ -758,6 +762,7 @@ if visible  then
 
 
                 if Mods == Mod and  r.ImGui_IsKeyPressed(ctx,  AllAvailableKeys[AftrLastPlus] )  then 
+                    msg('B'..v..'\n')
                     if Command_ID[i] then 
                         local Cmd_Num = r.NamedCommandLookup(Command_ID[i])
                         r.Main_OnCommand(Cmd_Num, 0)
